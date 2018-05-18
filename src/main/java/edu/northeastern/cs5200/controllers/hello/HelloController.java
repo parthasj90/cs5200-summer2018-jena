6213c208e370121f7ecc122fe8309e05ba8cd108
Partha Jena <jena.p@husky.neu.edu>
2018-05-18 18:22:03 -0400

    first commit

---
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -0,0 +1,32 @@
+
+package edu.northeastern.cs5200.controllers.hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class HelloController {
+
+ @RequestMapping("/api/hello/string")
+
+ public String sayHello() {
+
+  return "Hello Partha Jena!";
+
+ }
+
+ @RequestMapping("/api/hello/object")
+
+ public HelloObject sayHelloObject() {
+
+  HelloObject obj = new HelloObject("Hello Partha Jena!");
+
+  return obj;
+
+ }
+
+}
